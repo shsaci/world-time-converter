179071dac88c67b36951982d018badc44ed72c11
shsaci <008@saganbrainz.com>
2016-07-17 15:07:01 +1200

    commented code

--- code.js
+++ code.js
@@ -1,17 +1,20 @@
-document.addEventListener('DOMContentLoaded', myFunction)
+document.addEventListener('DOMContentLoaded', myFunction);
 
+// get user input from form
 function myFunction() {
   var form_element = document.getElementById('form1');
   var form_value = form_element.elements[0].value;
   searchURL(form_value);
 }
 
+// submit user input to google API to determine geo location codes
 function searchURL(value) {
   var searchUrl = "https://maps.googleapis.com/maps/api/geocode/json?address="+ value+"&key=AIzaSyDDNGnZS3lFLPw1DLC_5gAT5F_9hga1c-k";
   search(searchUrl);
   return searchUrl;
 }
 
+// interptret API response and extract latitude & longitude
 function search(locURL) {
   $.ajax({
     url: locURL,
@@ -20,19 +23,21 @@ function search(locURL) {
     success : function (data) {
       var obj = data;
       var o = obj.results[0]["geometry"]["location"];
-      // console.log(o.lat);console.log(o.lng);
       makeURL(o.lat, o.lng);
       initialize(o.lat, o.lng);
     }
   })
 }
 
+// construct URL to submit to google API to request time zone applicable in city
 function makeURL(lat, lng) {
   var url = "https://maps.googleapis.com/maps/api/timezone/json?location="+ lat + "," + lng + "&timestamp=1331161200&key=AIzaSyDDzmZaROsHfgXjz6hv4JuBs0eGcvh3HBI";
   timezn(url);
   return url;
 }
 
+// interpret and extract time zone from API response
+// Also print timezone to screen to help user
 function timezn(tzURL) {
   $.ajax({
     url: tzURL,
@@ -41,13 +46,12 @@ function timezn(tzURL) {
     success : function (data) {
       var obj = data;
       var tz = obj.timeZoneId;
-      console.log(tz);
       document.getElementById('tz').innerHTML=tz;
-      // newCityTz(tz);
     }
   })
 }
 
+// use time zone information to calculate (and display) local time in city
 function newCityTz() {
   var value = document.getElementById('tz').innerHTML;
   var time = moment()
@@ -56,23 +60,25 @@ function newCityTz() {
   document.getElementById('converted').innerHTML = "Local time: " + converted;
 }
 
-function updateClock(){
+// find and display NZ time
+function updateLocalClock(){
   var now = moment();
   var nz = now.format('MMMM Do YYYY, h:mm:ss a');
   document.getElementById('time').innerHTML = "NZ time: " + nz;
 }
 
+// Update local and new city clocks
 function timedUpdate () {
-  updateClock();
+  updateLocalClock();
   newCityTz();
   setTimeout(timedUpdate, 1000);
 }
 
 timedUpdate();
 
+// load map
 function initialize(lat, lng) {
   var mapOptions = {
-    // center: new google.maps.LatLng(-36.7700338,174.7002408),
     center: new google.maps.LatLng(lat,lng),
     zoom: 12,
     mapTypeId: google.maps.MapTypeId.ROADMAP
@@ -80,4 +86,3 @@ function initialize(lat, lng) {
 
   new google.maps.Map(document.getElementById('map'), mapOptions);
 }
-// google.maps.event.addDomListener(window, 'load', initialize);
